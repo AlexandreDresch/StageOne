bb1c1b50b0fd356d27aadd319a558f5bba5ee498
AlexandreDresch <connectWithAlexandre@proton.me>
2025-08-22 23:59:35 -0300

    feat: add Dashboard page & InterviewCard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,61 @@
+import InterviewCard from "@/components/interview-card";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions and get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start Practicing</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="Dashboard Illustration"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+
+          {dummyInterviews.length === 0 && (
+            <p className="text-slate-100/50">
+              You have no interviews.{" "}
+              <Link href="/interview" className="text-primary-100 underline">
+                Start practicing now!
+              </Link>
+            </p>
+          )}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+
+        <div className="interviews-section">
+          <p className="text-slate-100/50">
+            No interviews available. Please check back later.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ components/interview-card.tsx
@@ -0,0 +1,28 @@
+import dayjs from "dayjs";
+
+export default function InterviewCard({
+  interviewId,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MMM D, YYYY");
+
+  return (
+    <div className="card-border w-[360px] max-sm:full min-h-96">
+      <div className="card-interview">
+        <div>
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+            <p className="badge-text">{normalizedType}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
