8fde0f12d2ce98b75c3f1a8fbf741342b98b392c
AlexandreDresch <connectWithAlexandre@proton.me>
2025-09-01 23:45:44 -0300

    feat: add base interview page

---
+++ app/interview/layout.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/navbar";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function InterviewLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) {
+    redirect("/sign-in");
+  }
+  return (
+    <div className="root-layout">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
---
+++ app/interview/page.tsx
@@ -0,0 +1,11 @@
+import Agent from "@/components/agent";
+
+export default function Interview() {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="You" userId="user1" type="generate" />
+    </>
+  );
+}
